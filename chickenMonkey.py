523347ec6b35f4d48cb618fd1a22895ecae4ac25
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-15 09:39:43 -0600

    ready to start chicken monkey

---
+++ chickenMonkey.py
@@ -0,0 +1,9 @@
+# 1. Write a program that prints the numbers from 1 to 100. You can use Python's range() to quickly make a list of numbers.
+
+# 2. For multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number
+
+# 3. For the multiples of seven (7, 14, 21, etc.) print "Monkey".
+
+# 4. For numbers which are multiples of both five and seven print "ChickenMonkey".
+
+# 5. To determine if a number can be evenly divided by 5 or 7, use the Python modulo operator.
