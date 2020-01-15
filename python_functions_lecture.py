a0b2d63b963f5c5eda87365203ceadc996fc1711
Caroline Brownlee <brownleecaroline@gmail.com>
2020-01-15 05:20:25 -0600

    set up files for practice exercises

--- python_functions_lecture.py
+++ python_functions_lecture.py
@@ -14,8 +14,8 @@ def make_family(name, **brownlees):
 family = make_family("Brownlee", mom="Caroline", boy="Joseph")
 other_family = make_family("Madison", grandmother="Nana Martha")
 
-print(family)
-print(other_family)
+# print(family)
+# print(other_family)
 
 # FOR/IN LOOP
 stuff = ["caroline", "41", "hendersonville", "555-555-5555"]
@@ -32,6 +32,23 @@ cap_stuff = list(map(lambda foo: foo.capitalize(), stuff))
 
 # COMPREHENSION example (kind of how a ternary allows you to write a simple version of if/else...it's a shortened version of a for loop)
 cap_stuff = [foo.capitalize() for foo in stuff]
-print(cap_stuff)
+# print(cap_stuff)
+
+# COMPREHENSIONS can run on any collection. Here's a dictionary example:
+profile = {
+    "name": "Fred", 
+    "age": 34,
+    "address": "123 Sesame St."
+}
+
+#FOR/LOOP version
+profile_strings = []
+for key, value in profile.items():
+    profile_strings.append(f"The key is {key}. The value is {value}")
+
+# COMPREHENSION version
+profile_strings = [f"The key is {key}. The value is {value}" for key, value in profile.items()]
+
+print(profile_strings)
 
 
